839407911dd0dd04c1a8fd1ac64b34f738ab2ba5
Laughingtree <laughingtree.xm@gmail.com>
2024-09-21 12:00:45 +0800

    add comments

--- src/main.py
+++ src/main.py
@@ -20,6 +20,12 @@ def dec_str_to_bin_str(s):
 
 
 def compare_cmd_bit_flg(bit_a, bit_b):
+    """
+
+    :param bit_a: 待比较的第一个bitflg
+    :param bit_b: 待比较的第二个bitflg
+    :return: 第一个返回值表示待比较的bitflg是否一致，第二个返回值表示哪几个位数存在差异
+    """
     ignore_list = [4, 16]
     diffs = []
 
@@ -32,18 +38,19 @@ def compare_cmd_bit_flg(bit_a, bit_b):
     return len(diffs) == 0, diffs
 
 
-if __name__ == "__main__":
+def main_process(input, output):
+    """
+    主要处理函数，将bitflg差异信息写出到新的列
+    :param input: 输入的白盒对比结果差异文件.xlsx路径
+    :param output: 输出结果路径
+    :return:
+    """
     # 临时设置显示全部行
     pd.set_option('display.max_rows', None)
     # 临时设置显示全部列
     pd.set_option('display.max_columns', None)
 
-    df = pd.read_excel(r'..\whitebox-compare.xlsx')
-
-    rows, columns = df.shape
-    row_labels = df.index
-    column_labels = df.columns
-    print(row_labels, column_labels)
+    df = pd.read_excel(input, sheet_name='cli_final')
 
     df['old_bitFlg'] = df['old_info'].apply(lambda x: process_info(x))
     df['new_bitFlg'] = df['new_info'].apply(lambda x: process_info(x))
@@ -57,6 +64,7 @@ if __name__ == "__main__":
         new_flgs = row.new_bitFlg
         for i in range(0, len(old_flgs)):
             is_same, diffs = compare_cmd_bit_flg(old_flgs[i], new_flgs[i])
+            # 存在差异时，将差异写入此列，字典的key表示第几个bitflg存在差异，value表示该bitflg的哪几位存在差异
             if not is_same:
                 if df.iloc[index, bitFlg_diff_index] is None:
                     df.iloc[index, bitFlg_diff_index] = str({i: diffs})
@@ -66,6 +74,12 @@ if __name__ == "__main__":
                     df.iloc[index, bitFlg_diff_index] = str(new_val)
 
     print(df)
-    df.to_excel(r'..\result-0920.xlsx', index=False)
+    # df.to_excel(output, index=False)
+
+
+if __name__ == "__main__":
+    input_path = r'..\whitebox-compare.xlsx'
+    output_path = r'..\result-0920.xlsx'
+    main_process(input_path, output_path)
 
 
